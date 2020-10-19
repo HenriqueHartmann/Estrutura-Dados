f57bd0c1f34a593ffef1ba799e87b72b4b6c4815
Henrique Luiz Hartmann <alemaohartmann@gmail.com>
2020-10-19 19:46:03 -0300

    Add files via upload

--- celsius-para-fahrenheit.c
+++ celsius-para-fahrenheit.c
@@ -8,22 +8,28 @@
 /* Converte Celsius em Fahrenheit */
 float converte (float c)
 {
+	/* Declara a variavel f */
 	float f;
+	
+	/* Inicializa a variavel f com o valor da formula de conversao*/
 	f = c * 1.8 +32.0;
+	
+	/* Retorna f */
 	return f;
 }
 
 int main(void) 
 {
+	/* Declaracao das variaveis */
 	float t1, t2;
 	
 	/* Mostra mensagem para usuario */
 	printf("Digite a temperatura em Celsius: ");
 	
-	/* Captura valor de entrada via teclado */
+	/* Captura valor de entrada via teclado e atribui a t1 */
 	scanf("%f", &t1);
 	
-	/* Chama a function converte */
+	/* Chama a function converte e atribui seu valor a t2 */
 	t2 = converte(t1);
 	
 	/* Exibe o resultado */
--- maior-valor.c
+++ maior-valor.c
@@ -5,42 +5,65 @@
 	Turma: BSI4 
 */
 
+/* Escolhe o maior valor */
 float maior_valor(float a, float b, float c)
 {
+	/* Declara e inicializa a variavel m com valor de a */
 	float m = a;
 	
+	/* verifica se b é maior que m */
 	if (b > m)
 	{
+	 	/* Caso seja True, m recebe b */
 		m = b;
 	}
+	
+	/* verifica se c é maior que m */
 	if (c > m)
 	{
+		/* Caso seja True, m recebe c */
 		m = c;
 	}
 	
+	/* Retorna m */
 	return m;
 }
 
 int main(void) 
 {
+	/* Declaracao das variaveis */
 	float a,b,c,m;
 	
+	/* Mostra mensagem para o usuario */
 	printf("Digite 3 valores \n");
 	
 	printf("Primeiro valor: ");
-	scanf("%f", &a);
-	fflush(stdin);
 	
+	/* Captura o valor de entrada via teclado e atribui a variavel a*/
+	scanf("%f", &a);
+	
+	/* Limpa o buffer do teclado */
+	fflush(stdin);
+
 	printf("Segundo valor: ");
+	
+	/* Captura o valor de entrada via teclado e atribui a variavel b*/
 	scanf("%f", &b);
+	
+	/* Limpa o buffer do teclado */
 	fflush(stdin);
 	
 	printf("Terceiro valor: ");
+	
+	/* Captura o valor de entrada via teclado e atribui a variavel c*/
 	scanf("%f", &c);
 	
+	/* Chama a function maior_valor*/
 	m = maior_valor(a, b, c);
 	
+	/* Imprime o resultado */
 	printf("O maior valor e: %f\n", m);
 	
+	/* Termina o programa */
 	return 0;
 }
