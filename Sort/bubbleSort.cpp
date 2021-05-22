81eacf8465fbddef2bb0e89fd8fed208dc8de647
Henrique Luiz Hartmann <alemaohartmann@gmail.com>
2021-05-22 18:32:42 -0300

    Add files via upload

--- Sort/bubbleSort.cpp
+++ Sort/bubbleSort.cpp
@@ -1,28 +1,23 @@
 #include <stdio.h>
 
-int lenVet (int *vet);
-int check (int *vet);
-void bubbleSort (int *vet);
-void printVet (int *vet);
+int check (int *vet, int size);
+void bubbleSort (int *vet, int size);
+void printVet (int *vet, int size);
 
 int main()
 {
-	int vet[] = {5, 4, 2, 3};
-	bubbleSort(vet);
-	printVet(vet);
+	//int vet[] = {5, 4, 2, 3};
+	int vet[] = {100, 99, 98, 97, 96, 95, 94, 93, 92, 91, 90, 89, 88, 87, 86, 85, 84, 83, 82, 81, 80};
+	bubbleSort(vet, 21);
+	printVet(vet, 21);
 	
 	return 0;
 }
 
-int lenVet (int *vet)
-{
-	return sizeof(*vet);
-}
-
-int check (int *vet)
+int check (int *vet, int size)
 {
 	int correct = 0;
-	for (int i = 0; i < lenVet(vet) - 1; i++)
+	for (int i = 0; i < size - 1; i++)
 	{
 		if (vet[i] > vet[i+1])
 		{
@@ -34,12 +29,12 @@ int check (int *vet)
 	return correct;
 }
 
-void bubbleSort (int *vet)
+void bubbleSort (int *vet, int size)
 {
-	int correct = check(vet);
+	int correct = check(vet, size);
 	while (correct != 0)
 	{
-		for (int i = 0; i < lenVet(vet) - 1; i++)
+		for (int i = 0; i < size - 1; i++)
 		{
 			if (vet[i] > vet[i+1])
 			{
@@ -48,13 +43,13 @@ void bubbleSort (int *vet)
 				vet[i+1] = aux;
 			}
 		}
-		correct = check(vet);
+		correct = check(vet, size);
 	}
 }
 
-void printVet (int *vet)
+void printVet (int *vet, int size)
 {
-	for (int i = 0; i < lenVet(vet); i++)
+	for (int i = 0; i < size; i++)
 	{
 		printf("%d ", vet[i]);
 	}
